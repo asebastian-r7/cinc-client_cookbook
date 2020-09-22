c91f7b8c011a9b2b7230d20005f1864e57448472
atrull <4817255+atrull@users.noreply.github.com>
2020-09-22 22:49:23 +0200

    defaults are good enough. license. doco.

--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,8 @@ name             'cinc-client'
 maintainer       'Alex Trull'
 maintainer_email 'alex@trull.org'
 license          'All rights reserved'
-description      'Shims chef-client cookbook for the Cinc project'
+description      'Shims chef-client cookbook to work with the Cinc-Client paths'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.4'
 
 depends "chef-client"
