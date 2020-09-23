4396fc67775d2b7307472054058908b0695fcdc0
atrull <4817255+atrull@users.noreply.github.com>
2020-09-23 17:10:13 +0200

    correct issues url

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Shims chef-client cookbook to work with the Cinc-Client paths'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.5'
 source_url       'https://github.com/atrull/cinc-client_cookbook'
-issues_url       'https://github.com/atrull/cinc-client/issues'
+issues_url       'https://github.com/atrull/cinc-client_cookbook/issues'
 depends          "chef-client"
 %w( amazon centos fedora freebsd debian redhat suse opensuseleap ubuntu windows).each do |os|
   supports os
