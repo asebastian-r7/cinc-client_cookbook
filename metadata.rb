b7d9929c507d144e8d6f87f452887045fe06ef9f
atrull <4817255+atrull@users.noreply.github.com>
2020-09-23 17:11:22 +0200

    bumping the version.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'alex@trull.org'
 license          'BSD V3'
 description      'Shims chef-client cookbook to work with the Cinc-Client paths'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.5'
+version          '0.0.6'
 source_url       'https://github.com/atrull/cinc-client_cookbook'
 issues_url       'https://github.com/atrull/cinc-client_cookbook/issues'
 depends          "chef-client"
